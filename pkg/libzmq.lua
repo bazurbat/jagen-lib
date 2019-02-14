42ad68f0a5211066127080352c01770d92c8e3cf
Oleg Kolosov <bazurbat@gmail.com>
2019-02-14 13:28:55 +0100

    Add libzmq pkg

---
+++ pkg/libzmq.lua
@@ -0,0 +1,9 @@
+return {
+    source = {
+        -- location = 'https://github.com/zeromq/libzmq.git',
+        -- tag = 'v4.2.5'
+        location = 'https://github.com/zeromq/libzmq/releases/download/v4.3.1/zeromq-4.3.1.tar.gz',
+        sha1sum = '6cce22d830eaf95feff7cab00744df13ad7ab7f3'
+    },
+    build = 'cmake'
+}
