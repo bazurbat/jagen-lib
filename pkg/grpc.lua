3da055b5a899746b381ec84ddaa278f3784b0ed3
Oleg Kolosov <bazurbat@gmail.com>
2020-12-07 20:51:29 +0100

    Bump gRPC version to 1.33.x

--- pkg/grpc.lua
+++ pkg/grpc.lua
@@ -1,16 +1,20 @@
 return {
     source = {
         type     = 'git',
-        location = 'https://github.com/bazurbat/grpc.git',
-        tag      = 'v1.6.x',
+        location = 'https://github.com/grpc/grpc',
+        branch   = 'v1.33.x',
     },
     build = {
-        type = 'make'
-    },
-    install = {
-        args = { 'prefix=$pkg_install_dir' }
-    },
-    requires = {
-        'protobuf'
+        type = 'cmake',
+        options = {
+            '-DgRPC_INSTALL=ON',
+            '-DgRPC_BUILD_TESTS=OFF',
+            '-DgRPC_ABSL_PROVIDER=package',
+            '-DgRPC_CARES_PROVIDER=package',
+            '-DgRPC_PROTOBUF_PROVIDER=package',
+            '-DgRPC_RE2_PROVIDER=package',
+            '-DgRPC_SSL_PROVIDER=package',
+            '-DgRPC_ZLIB_PROVIDER=package'
+        }
     }
 }
